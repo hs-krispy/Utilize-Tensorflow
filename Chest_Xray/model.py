480de4c760ef3f8b7ba6d7706018254cbfdd7a37
hs-krispy <58063806+hs-krispy@users.noreply.github.com>
2020-12-16 23:12:28 +0900

    CNN model

--- Chest_Xray/model.py
+++ Chest_Xray/model.py
@@ -2,56 +2,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.utils import np_utils
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout, BatchNormalization
+from tensorflow.keras.layers import Flatten, Dense, SeparableConv2D, MaxPooling2D, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
-from keras.preprocessing.image import image, ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 
-X_train = np.load('D:/dataset/train/X_train.npy')
-y_train = np.load('D:/dataset/train/y_train.npy')
-X_val = np.load('D:/dataset/val/X_val.npy')
-y_val = np.load('D:/dataset/val/y_val.npy')
-X_test = np.load('D:/dataset/test/X_test.npy')
-y_test = np.load('D:/dataset/test/y_test.npy')
+X_train = np.load('D:/dataset/train/X_train(150_gray).npy')
+y_train = np.load('D:/dataset/train/y_train(150_gray).npy')
+X_val = np.load('D:/dataset/val/X_val(150_gray).npy')
+y_val = np.load('D:/dataset/val/y_val(150_gray).npy')
+X_test = np.load('D:/dataset/test/X_test(150_gray).npy')
+y_test = np.load('D:/dataset/test/y_test(150_gray).npy')
 
 y_train = np_utils.to_categorical(y_train, 2)
 y_val = np_utils.to_categorical(y_val, 2)
 y_test = np_utils.to_categorical(y_test, 2)
 
+epoch = 50
+batch_size = 16
 seed = 100
 np.random.seed(seed)
 
+generator = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, height_shift_range=0.1,
+                               zoom_range=0.3, vertical_flip=True, horizontal_flip=True)
+train_data = generator.flow(X_train, y_train, batch_size=batch_size, seed=seed)
+
 model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(150, 150, 3), padding="same", activation="relu", kernel_initializer="he_uniform"))
-model.add(Dropout(0.5))
-model.add(BatchNormalization())
-model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
-model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(SeparableConv2D(16, kernel_size=(3, 3), input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3]), padding="same", activation="relu", kernel_initializer="he_uniform"))
+model.add(SeparableConv2D(32, kernel_size=(3, 3), padding="same", activation="relu", kernel_initializer="he_uniform"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+
 model.add(BatchNormalization())
-model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
-model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(SeparableConv2D(64, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
 model.add(BatchNormalization())
-model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
-model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(SeparableConv2D(64, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(BatchNormalization())
+model.add(SeparableConv2D(64, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
+
+model.add(BatchNormalization())
+model.add(SeparableConv2D(128, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(BatchNormalization())
+model.add(SeparableConv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(BatchNormalization())
+model.add(SeparableConv2D(128, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(BatchNormalization())
+model.add(SeparableConv2D(256, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(BatchNormalization())
+model.add(SeparableConv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(BatchNormalization())
+model.add(SeparableConv2D(256, kernel_size=(1, 1), padding='same', activation='relu', kernel_initializer="he_uniform"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
 model.add(Flatten())
-model.add(BatchNormalization())
+model.add(Dense(1024, activation='relu', kernel_initializer="he_uniform"))
 model.add(Dense(512, activation='relu', kernel_initializer="he_uniform"))
 model.add(Dropout(0.5))
-model.add(BatchNormalization())
 model.add(Dense(256, activation='relu', kernel_initializer="he_uniform"))
-model.add(Dense(2, activation='softmax'))
-model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.add(Dense(2, activation='sigmoid'))
+model.compile(optimizer=Adam(lr=0.0001, decay=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=32, verbose=1)
+model.summary()
 
-print('\nTrain accuracy: {:.4f}\nTest accuracy: {:.4f}'.format(model.evaluate(X_train, y_train)[1], model.evaluate(X_test, y_test)[1]))
-# Train accuracy - 0.9918
-# Test accuracy - 0.8093
+callbacks = [ModelCheckpoint(filepath='best_model.h5', verbose=1, monitor='acc', save_best_only=True)]
+history = model.fit(train_data, steps_per_epoch=len(X_train) // batch_size, epochs=epoch, verbose=1,
+                    class_weight={0:3.0, 1:1.0}, validation_data=(X_val, y_val), callbacks=callbacks)
+print('\nTest loss: {:.4f}\nTest accuracy: {:.4f}'.format(model.evaluate(X_test, y_test)[0], model.evaluate(X_test, y_test)[1]))
 
 plt.title('model accuracy')
 plt.plot(history.history['acc'])
@@ -60,5 +81,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'])
 plt.show()
+
+model.save('model.h5')
 
 
